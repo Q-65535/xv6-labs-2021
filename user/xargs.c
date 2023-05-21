4f151f4e26a3f2419c8784bfac50b64cc66ebc38
Di Wu <g20162620307@gmail.com>
2023-05-21 06:15:38 -0700

    finish xargs

---
+++ user/xargs.c
@@ -0,0 +1,50 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/fs.h"
+#include "kernel/param.h"
+
+#define stdin 0
+#define MAX_ARG_LEN 100
+
+int read_line(char *arg) {
+	int index = 0;
+	char buf;
+	// Read bytes one by one.
+	while(read(stdin, &buf, 1) == 1) {
+		// The return character means that a command line is complete.
+		if (buf == '\n') {
+			arg[index] = '\0';
+			return 1;
+		} else {
+			arg[index++] = buf;
+		}
+	}
+	return -1;
+}
+
+int main(int argc, char *argv[]) {
+	char *args[MAXARG];
+	char arg[MAX_ARG_LEN];
+	// Copy the prefix args.
+	int index = 0;
+	for (; index < argc - 1; index++) {
+		args[index] = argv[index + 1];
+	}
+	while (read_line(arg) != -1) {
+		args[index] = arg;
+		args[index + 1] = 0;
+
+		int pid = fork();
+		// parent process
+		if (pid != 0) {
+			wait(0);
+			// child process
+		} else {
+			int res = exec(args[0], (char**)args);
+			printf("exe fail res: %d\n", res);
+		}
+
+	}
+	exit(0);
+}
