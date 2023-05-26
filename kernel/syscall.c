8829f495528ad1999ae761d1ca76b3e87890943d
Di Wu <g20162620307@gmail.com>
2023-05-26 09:53:27 -0700

    finish trace lab

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -7,6 +7,32 @@
 #include "syscall.h"
 #include "defs.h"
 
+static char *syscalls_name[] = {
+	"",
+    "fork",
+	"exit",
+	"wait",
+	"pipe",
+	"read",
+	"kill",
+	"exec",
+	"fstat",
+	"chdir",
+	"dup",
+	"getpid",
+	"sbrk",
+	"sleep",
+	"uptime",
+	"open",
+	"write",
+	"mknod",
+	"unlink",
+	"link",
+	"mkdir",
+	"close",
+	"trace"
+};
+
 // Fetch the uint64 at addr from the current process.
 int
 fetchaddr(uint64 addr, uint64 *ip)
@@ -104,6 +130,7 @@ extern uint64 sys_unlink(void);
 extern uint64 sys_wait(void);
 extern uint64 sys_write(void);
 extern uint64 sys_uptime(void);
+extern uint64 sys_trace(void);
 
 static uint64 (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
@@ -127,6 +154,7 @@ static uint64 (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
+[SYS_trace]   sys_trace,
 };
 
 void
@@ -138,6 +166,10 @@ syscall(void)
   num = p->trapframe->a7;
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
     p->trapframe->a0 = syscalls[num]();
+	// trace
+	if ((1 << num) & p->mask) {
+	  printf("%d: syscall %s -> %d\n", p->pid, syscalls_name[num], p->trapframe->a0);
+	}
   } else {
     printf("%d %s: unknown sys call %d\n",
             p->pid, p->name, num);
--- user/user.h
+++ user/user.h
@@ -23,6 +23,7 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+int trace(int); // system call lab-1
 
 // ulib.c
 int stat(const char*, struct stat*);
